bd5e399a40b9bc3557f64054000aa1dbf9b5712c
kaz_kolyan97@mail.ru <kaz_kolyan97@mail.ru>
2021-08-21 22:43:41 +0300

    Добавьте файлы проекта.

---
+++ unidirectional_list/unidirectional_list.cpp
@@ -0,0 +1,13 @@
+﻿#include "Class_list.h" // "" - мой класс
+#include <iostream> // <> - стандартная библиотека
+
+using namespace std;
+
+int main()
+{
+
+    List N;
+    system("pause");
+    return 0;
+}
+
