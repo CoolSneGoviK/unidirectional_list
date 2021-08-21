61201455fd80e3f832826f78a285bc3f29ff7334
kaz_kolyan97@mail.ru <kaz_kolyan97@mail.ru>
2021-08-21 23:26:27 +0300

    добавил метод list - добавление элементов в h. в главном файле добавил 1 элемент

--- unidirectional_list/Class_list.h
+++ unidirectional_list/Class_list.h
@@ -6,20 +6,37 @@ class List
 public:
 	List(); // объявление, конструктор по умолчанию ничего не делает
 	//~List();
+	void add_element(T data); // добавление элемента
 
 private:
 
 	template <class T>
 	class Node
 	{ // узел - из узлов состоит список
+	public:
+		Node(T data = T(), Node<T>* next = nullptr) // принимает данные и указатель на следующий узел в списке
+		{
+			this->data = data;
+		}
+
 		Node* Next;
 		T data;
 	};
 
-	Node* head;
-	int Size;
+	Node<T>* head = nullptr;
+	int Size = 0;
 };
 
 template <class T>
-List<T>::List () 
-{} // определение - пустое, по умолчанию ничего не происходит
+List<T>::List()
+{}; // определение - пустое, по умолчанию ничего не происходит
+
+template<class T>
+void List<T>::add_element(T data)
+{
+	if (this->head == nullptr)
+	{
+		this->head = new Node<T>(data);
+		this->Size++;
+	}
+};
--- unidirectional_list/unidirectional_list.cpp
+++ unidirectional_list/unidirectional_list.cpp
@@ -7,6 +7,7 @@ int main()
 {
 
     List<int> N; // класс подключился, тут выбрал тип int 
+    N.add_element(10); // добавили 1 элемент 
     system("pause");
     return 0;
 }
